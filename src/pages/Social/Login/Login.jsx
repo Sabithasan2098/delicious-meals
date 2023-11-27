af6ca5f472996a3cf0b2f346541bbc7b40fe935c
Sabithasan2098 <sabithasan2098@gmail.com>
2023-11-27 20:37:27 +0600

    login register

---
+++ src/pages/Social/Login/Login.jsx
@@ -0,0 +1,61 @@
+import { Link } from "react-router-dom";
+import login from "../../../../public/login-register.avif";
+
+const Login = () => {
+  return (
+    <div className="flex md:flex-row sm:flex-row ">
+      <div className="">
+        <img src={login} alt="" />
+      </div>
+      <div className="hero min-h-screen lg:absolute -top-24">
+        <div className="hero-content flex-col ">
+          <div className="text-center ">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+          </div>
+          <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button className="btn btn-primary">Login</button>
+              </div>
+              <p className="text-md font-bold text-[#001F3F]">
+                Do not have an account.Please{" "}
+                <Link to={"/register"} className="underline text-yellow-400">
+                  Register
+                </Link>
+              </p>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
